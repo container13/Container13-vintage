5f1b3dd0382eeeac8233782be0b7a4f41c2c798e
container13 <nilsmagnusalvin@gmail.com>
2026-07-27 23:48:37 +0200

    Add files via upload

--- admin/sw.js
+++ admin/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "container13-admin-v6.10.9";
+const CACHE_NAME = "container13-admin-v6.10.10";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./panel.html?v=6.10.39",
-  "./pwa.js?v=6.10.39",
+  "./panel.html?v=6.10.40",
+  "./pwa.js?v=6.10.40",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
