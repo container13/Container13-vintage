8e7a1d83d6589a14dabd0bc3b0c80c22793235f0
container13 <nilsmagnusalvin@gmail.com>
2026-08-03 09:47:21 +0200

    Add files via upload

--- ccc-core/demo-ui/app.js
+++ ccc-core/demo-ui/app.js
@@ -2,4 +2,4 @@ function showView(id){
 document.querySelectorAll('.view').forEach(v=>v.hidden=true);
 document.getElementById(id).hidden=false;
 }
-showView('confirmView');
+showView('welcomeView');
