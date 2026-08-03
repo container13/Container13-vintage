970bd164095aaa4209112911ce232ad5386c0855
container13 <nilsmagnusalvin@gmail.com>
2026-08-03 08:24:26 +0200

    Add files via upload

--- ccc-core/demo-ui/app.js
+++ ccc-core/demo-ui/app.js
@@ -1,11 +1,10 @@
-function showView(viewId) {
-  document.querySelectorAll(".view").forEach(view => {
-    view.classList.remove("active");
-  });
-
-  document.getElementById(viewId).classList.add("active");
+function showView(viewId){
+document.querySelectorAll('.view').forEach(view=>{
+view.classList.remove('active');
+});
+document.getElementById(viewId).classList.add('active');
 }
 
-document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+document.getElementById('themeToggle').addEventListener('click',()=>{
+document.body.classList.toggle('dark');
 });
