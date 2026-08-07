d53da72d381ce97ea6c65f475dc979c48e083f2e
container13 <nilsmagnusalvin@gmail.com>
2026-08-07 07:26:51 +0200

    Delete ccc/demo-ui/app.js

--- ccc/demo-ui/app.js
+++
@@ -1,7 +0,0 @@
-const saveButton = document.getElementById("save");
-const theme = document.getElementById("theme");
-const message = document.getElementById("message");
-
-saveButton.addEventListener("click", () => {
-  message.textContent = "Configuration sparad (demo)";
-});
