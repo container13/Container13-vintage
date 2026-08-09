3a6ad06a74b2865d0437c8e6e56fca4a740827c4
container13 <nilsmagnusalvin@gmail.com>
2026-08-09 05:01:25 +0200

    Add files via upload

--- ccc-core/demo-ui/installningar.js
+++ ccc-core/demo-ui/installningar.js
@@ -1,4 +1,4 @@
 (() => {
-  // v2.7.23: Den här sidan innehåller endast generella CCC-inställningar.
+  // v2.7.24: Den här sidan innehåller endast generella CCC-inställningar.
   // Modulspecifika inställningar ligger i respektive moduls kugghjul.
 })();
