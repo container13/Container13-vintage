bbb1535267899b634043039e62e5ce527b563dd4
container13 <nilsmagnusalvin@gmail.com>
2026-08-21 01:32:55 +0200

    Add files via upload

--- ccc-core/version.js
+++ ccc-core/version.js
@@ -1,5 +1,5 @@
 (() => {
-  const CCC_VERSION = "2.9.84";
+  const CCC_VERSION = "2.9.85";
   window.CCC_VERSION = CCC_VERSION;
   function applyVersion(){
     document.querySelectorAll(".js-ccc-version").forEach(el=>el.textContent=`v${CCC_VERSION}`);
